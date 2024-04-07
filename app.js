1924497b3e725b9ec253985a598cc65154f515f7
mhesemans <micha_hesemans@hotmail.com>
2024-04-07 20:49:16 +0100

    feat: added more shapes to app.js

--- app.js
+++ app.js
@@ -7,13 +7,41 @@ document.addEventListener ("DOMContentLoaded", () =>{
     console.log(squares)
 
     //shapes
-    const lShape = [
+    const lShape = [ // L-Shape
         [1, width+1,width*2+1,2],
         [width, width+1, width+2, width*2+2],
         [1, width+1,width*2+1,width*2],
         [width, width*2, width*2+1, width*2+2]
     ]
-    
-    const 
+
+    const bShape = [ // Block Shape
+        [1,width+1,2,width+2],
+        [1,width+1,2,width+2],
+        [1,width+1,2,width+2],
+        [1,width+1,2,width+2]
+    ]
+
+    const longShape = [ //Long (bar) Shape
+        [1, width+1, width*2+1, width*3+1],
+        [width, width+1, width+2, width+3],
+        [1, width+1, width*2+1, width*3+1],
+        [width, width+1, width+2, width+3]
+    ]
+
+    const zShape = [ //z-Shape
+        [1,2,width+1,width+2],
+        [1,width+1,width,width*2],
+        [1,2,width+1,width+2],
+        [1,width+1,width,width*2]
+    ]
+
+    const tShape = [ //T-Shape
+        [1,2,3,width+2],
+        [1,width+1, width*2+1,width+2],
+        [2,width+1,width+2, width+3],
+        [2,width+2,width*2+2, width+1]
+    ]
+    //Array that contains all shapes
+    const allShapes = [lShape,longShape, zShape, bShape, tShape]
 }
 )
