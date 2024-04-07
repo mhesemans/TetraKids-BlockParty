7249fd8d4b09161e54587a78aa4975852da424e5
mhesemans <micha_hesemans@hotmail.com>
2024-04-07 19:42:52 +0100

    feat: added variables for grid, score and start button

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 document.addEventListener ("DOMContentLoaded", () =>{
-
+    const grid = document.querySelector(".grid")
+    const score = document.getElementById("score")
+    const startBtn = document.getElementById("start_btn")
+    let squares = Array.from(document.querySelectorAll(".grid"))
+    const width = 10
+    console.log(squares)
 
 }
 )
