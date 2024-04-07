5a38bd8b1c5c01d83dbfea787377a9642d264bf9
mhesemans <micha_hesemans@hotmail.com>
2024-04-07 20:04:24 +0100

    feat: created lShape

--- app.js
+++ app.js
@@ -3,8 +3,17 @@ document.addEventListener ("DOMContentLoaded", () =>{
     const score = document.getElementById("score")
     const startBtn = document.getElementById("start_btn")
     let squares = Array.from(document.querySelectorAll(".grid"))
-    const width = 10
+    const width = 12
     console.log(squares)
 
+    //shapes
+    const lShape = [
+        [1, width+1,width*2+1,2],
+        [width, width+1, width+2, width*2+2],
+        [1, width+1,width*2+1,width*2],
+        [width, width*2, width*2+1, width*2+2]
+    ]
+    
+    const 
 }
 )
